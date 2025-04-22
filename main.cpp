ccca4ea6fd6478d361474ea357ccacd75dfc6f0e
Saakshi Kobarne <saakshik15@gmail.com>
2025-04-22 17:58:24 +0530

    Initial setup files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include <QLabel>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QLabel label("Hello, World!");
+    label.setWindowTitle("My 100th Qt Application");
+    label.resize(200, 100);
+    label.show();
+
+    return app.exec();
+}
